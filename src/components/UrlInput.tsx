ad88f2f931aa0439a9eb05a809b43cc63127705f
whl <whlit@aliyun.com>
2024-01-03 17:25:52 +0800

    feat: url输入框

---
+++ src/components/UrlInput.tsx
@@ -0,0 +1,54 @@
+function UrlInput({
+  method,
+  onChangeMethod,
+  url,
+  onChangeUrl,
+  send,
+}: {
+  method: string
+  onChangeMethod: React.ChangeEventHandler<HTMLSelectElement>
+  url: string
+  onChangeUrl: React.ChangeEventHandler<HTMLInputElement>
+  send: React.MouseEventHandler<Element>
+}) {
+  const options = [
+    { key: 'GET', label: 'GET' },
+    { key: 'POST', label: 'POST' },
+  ]
+  return (
+    <>
+      <div style={{ display: 'flex', alignItems: 'center', width: '1000px' }}>
+        <div
+          style={{
+            border: '1px solid #dfe1e5',
+            borderRadius: '5px',
+            flexGrow: 1,
+            display: 'flex',
+          }}>
+          <select
+            name='method'
+            title='method'
+            style={{ border: '0', margin: '8px', outline: 'none' }}
+            value={method}
+            onChange={onChangeMethod}>
+            {options.map((option) => (
+              <option value={option.key}>{option.label}</option>
+            ))}
+          </select>
+          <input
+            name='url'
+            title='url'
+            style={{ border: '0', outline: 'none', margin: '8px 2px', flexGrow: 1 }}
+            placeholder='url'
+            value={url}
+            onChange={onChangeUrl}
+          />
+        </div>
+        <button style={{ outline: 'none', marginLeft: '3px' }} onClick={send}>
+          发送
+        </button>
+      </div>
+    </>
+  )
+}
+export default UrlInput
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,46 @@
+import { useImmerReducer } from 'use-immer'
+import UrlInput from '../components/UrlInput'
+
+interface State {
+  method: string
+  url: string
+}
+enum ActionType {
+  C_METHOD,
+  C_URL,
+}
+type Action =
+  | {
+      type: ActionType.C_METHOD
+      method: State['method']
+    }
+  | {
+      type: ActionType.C_URL
+      url: State['url']
+    }
+
+const reducer = (draft: State, action: Action) => {
+  switch (action.type) {
+    case ActionType.C_METHOD:
+      draft.method = action.method
+      break
+    case ActionType.C_URL:
+      draft.url = action.url
+      break
+  }
+}
 function Home() {
-  return <>Home</>
+  const [data, dispatch] = useImmerReducer(reducer, { method: 'GET', url: '' })
+  return (
+    <>
+      <UrlInput
+        method={data.method}
+        onChangeMethod={(v) => dispatch({ type: ActionType.C_METHOD, method: v.target.value })}
+        url={data.url}
+        onChangeUrl={(v) => dispatch({ type: ActionType.C_URL, url: v.target.value })}
+        send={(v) => alert(data)}
+      />
+    </>
+  )
 }
 export default Home
